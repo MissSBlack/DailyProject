b5336e08c733997cc5592232bab5583508ac2ba9
qianxiao999 <1035497424@qq.com>
2020-01-03 10:24:46 +0800

    框架搭建20200103！！！！！

---
+++ src/test/java/com/adc/daily/DailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adc.daily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
